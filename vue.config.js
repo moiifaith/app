9baf0fc19c9269c5754e90d3b3342491278091fc
Amar Bešlija <amar.beslija@lab387.com>
2025-09-21 19:54:57 +0000

    Allow Vue to run on codespaces

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    client: {
+      webSocketURL: 'auto://0.0.0.0:0/ws'
+    },
+    allowedHosts: 'all',
+    host: '0.0.0.0',
+    port: 8080,
+    https: false,
+    hot: true,
+    liveReload: true
+  }
 })
